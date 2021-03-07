0e14d944e69ff60af1315de54a6f382140b1102c
Haouah19 <ahmed_haouili@yahoo.fr>
2021-03-07 18:59:59 +0000

    dynamic libs added

--- main/main.cpp
+++ main/main.cpp
@@ -5,34 +5,43 @@
 int main(int argc, char ** argv)
 {
 
+	printf("Starting...");
+
 	void *libComposant1;
 	void *libComposant2;
 
-	libComposant1 = dlopen ("libComposant1.so", RTLD_LOCAL|RTLD_LAZY);
+	libComposant1 = dlopen ("libComposant1.so", RTLD_LAZY);
 	if (!libComposant1) {
 		printf ("Erreur dlopen: %s\n", dlerror());
-        	exit(1);
+        	exit(EXIT_FAILURE);
     	}
 
-	libComposant2 = dlopen ("libComposant2.so", RTLD_LOCAL|RTLD_LAZY);
+	libComposant2 = dlopen ("libComposant2.so", RTLD_LAZY);
 	if (!libComposant2) {
 		printf ("Erreur dlopen: %s\n", dlerror ());
-		exit(1);
+		exit(EXIT_FAILURE);
     	}
+
+	int (*composant1)(int, int) = (int (*)(int, int))dlsym(libComposant1, "composant1");
+	if (!composant1) {
+                printf ("Erreur dlsym: %s\n", dlerror());
+                exit(EXIT_FAILURE);
+        }
+	int (*composant2)(int, int) = (int (*)(int, int))dlsym(libComposant2, "composant2");
+	if (!composant2) {
+                printf ("Erreur dlsym: %s\n", dlerror());
+                exit(EXIT_FAILURE);
+        }
+	char *(*getComposant1Version)() = (char *(*)())dlsym(libComposant1, "getComposant1Version");
+	if (!getComposant1Version) {
+                printf ("Erreur dlsym: %s\n", dlerror());
+                exit(EXIT_FAILURE);
+        }
 
 	int data1=3;
 	int data2=5;
-
-	int (*composant1)(int, int) = (int (*)(int, int))dlsym(libComposant1, "composant1");
-	int (*composant2)(int, int) = (int (*)(int, int))dlsym(libComposant2, "composant2");
-	char *(*getComposant1Version)(void) = (char *(*)(void))dlsym(libComposant1, "getComposant1Version");
-
-	int valeur1;
-	int valeur2;
-
-	valeur1=composant1(data1,data2);
-
-	valeur2=composant2(data1,data2);
+	int valeur1=composant1(data1,data2);
+	int valeur2=composant2(data1,data2);
 
 	std::cout << getComposant1Version() << std::endl;
 	std::cout << "valeur 1 :" << valeur1 << " valeur 2 :" << valeur2 << std::endl;
